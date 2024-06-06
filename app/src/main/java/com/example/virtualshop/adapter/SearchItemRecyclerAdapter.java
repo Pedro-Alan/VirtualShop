109b4c9e7d60822715de206d5203758830c49226
alanz <alanzito006@gmail.com>
2024-06-06 11:24:31 -0300

    FirestoreRecyclerAdapter added

--- app/src/main/java/com/example/virtualshop/adapter/SearchItemRecyclerAdapter.java
+++ app/src/main/java/com/example/virtualshop/adapter/SearchItemRecyclerAdapter.java
@@ -1,14 +1,34 @@
 package com.example.virtualshop.adapter;
 
+import android.content.Context;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.virtualshop.R;
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 
-public class SearchItemRecyclerAdapter {
+public class SearchItemRecyclerAdapter extends FirestoreRecyclerAdapter<UserModel, SearchItemRecyclerAdapter, SearchItemRecyclerAdapter.ItemModelViewHolder> {
+    Context context;
+    public SearchItemRecyclerAdapter(@NonNull FirestoreRecyclerOptions<UserModel> options) {
+        super(options);
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull SearchItemRecyclerAdapter holder, int position, @NonNull UserModel model) {
+
+    }
+
+    @NonNull
+    @Override
+    public SearchItemRecyclerAdapter onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
     class ItemModelViewHolder extends RecyclerView.ViewHolder {
         TextView itemNameView, itemDescView;
         ImageView itemImgView;
